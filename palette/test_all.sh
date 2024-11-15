21db1b2ad2740b59f75964fe88fffafc8642749b
Faruk <farukoruc1@gmail.com>
2024-11-15 22:06:57 +0100

    Remove json files

--- palette/test_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python run.py -p test -c config/inpainting_u_maze_64_test_large_black.json
-python run.py -p test -c config/inpainting_u_maze_64_test_small_black.json
-python run.py -p test -c config/inpainting_u_maze_64_test_large.json
-python run.py -p test -c config/inpainting_u_maze_64_test.json
--- palette/train_all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python run.py -p train -c config/inpainting_u_maze_64_large_black_horizontal.json
-python run.py -p train -c config/inpainting_u_maze_64_large_black.json
