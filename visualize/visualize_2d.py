6a5c0c17254266e693d1701de24acffda8583e4b
Erdi Sayar <ser@vmknoll81.in.tum.de>
2025-05-02 14:51:39 +0200

    Update Visualization

--- visualize/visualize_2d.py
+++ visualize/visualize_2d.py
@@ -129,15 +129,22 @@ class SpiralMaze(object):
 
 
 def aim_visualize(class_instance,goal_candidates, agent, savedir_w_name):
-    number_of_points = 100
-    x_debug = torch.linspace(class_instance.uniform_goal_sampler.LOWER_CONTEXT_BOUNDS[0],
-                             class_instance.uniform_goal_sampler.UPPER_CONTEXT_BOUNDS[0], number_of_points)
-    y_debug = torch.linspace(class_instance.uniform_goal_sampler.LOWER_CONTEXT_BOUNDS[1],
-                             class_instance.uniform_goal_sampler.UPPER_CONTEXT_BOUNDS[1], number_of_points)
-    X_debug, Y_debug = torch.meshgrid(x_debug, y_debug)
-    x_input_debug = X_debug.reshape(-1, 1)
-    y_input_debug = Y_debug.reshape(-1, 1)
-    virtual_diffusion_goals_debug = torch.hstack((x_input_debug, y_input_debug)).to(class_instance.device)
+    num_test_points = 100  # 30
+    if class_instance.cfg.env in ['AntMazeSmall-v0', "PointUMaze-v0"]:
+        x = np.linspace(-2, 10, num_test_points)
+        y = np.linspace(-2, 10, num_test_points)
+    elif class_instance.cfg.env in ['sawyer_peg_push']:
+        x = np.linspace(-0.6, 0.6, num_test_points)
+        y = np.linspace(0.2, 1.0, num_test_points)
+    elif class_instance.cfg.env == "PointSpiralMaze-v0":
+        x = np.linspace(-10, 10, num_test_points)
+        y = np.linspace(-10, 10, num_test_points)
+    elif class_instance.cfg.env in ["PointNMaze-v0"]:
+        x = np.linspace(-2, 10, num_test_points)
+        y = np.linspace(-2, 18, num_test_points)
+    else:
+        return None
+
     if class_instance.cfg.env == "PointSpiralMaze-v0":
         background_image_path = os.path.join(os.path.dirname(__file__), 'PointSpiralMaze_background.png')
         world_coordinate = np.float32([[8, -8], [-8, -8], [-8, 8], [8, 8]])
@@ -161,146 +168,71 @@ def aim_visualize(class_instance,goal_candidates, agent, savedir_w_name):
         raise NotImplementedError
 
 
-    for goal_index, selected_goal in enumerate(goal_candidates):
+    grid_x, grid_y = np.meshgrid(x, y)
+    goal_xy = np.concatenate([np.reshape(grid_x, [-1, 1]), np.reshape(grid_y, [-1, 1])],
+                             axis=1)  # [num_test_points^2, 2]
 
-        fig, ax = plt.subplots()
-        fig2, ax2 = plt.subplots()
-        fig3, ax3 = plt.subplots()
-        fig4, ax4 = plt.subplots()
+    if class_instance.cfg.env in ['sawyer_peg_push']:
+        goal_xy = np.concatenate([goal_xy, 0.01457 * np.ones([goal_xy.shape[0], 1])], axis=-1)  # [num_test_points^2, 3]
 
-        repeated_state_debug = torch.tile(torch.tensor(selected_goal).to(class_instance.device),
-                                          [virtual_diffusion_goals_debug.shape[0], 1])
-        initial_state = np.zeros_like(selected_goal)
-        repeated_initial_state = torch.tile(torch.tensor(initial_state).to(class_instance.device),[virtual_diffusion_goals_debug.shape[0], 1])
+    num_grid_point = goal_xy.shape[0]
 
-        inputs_norm_tensor_tmp = torch.hstack((virtual_diffusion_goals_debug, repeated_state_debug))
-        inputs_norm_tensor_tmp_initial = torch.hstack((repeated_initial_state, virtual_diffusion_goals_debug))
+    initial_states = np.tile(initial_state, (num_grid_point, 1))  # [num_test_points^2, dim]
 
-        if class_instance.cfg.normalize_rl_obs:
-            inputs_norm_tensor_tmp = agent.normalize_obs(inputs_norm_tensor_tmp, class_instance.cfg.env)
-            inputs_norm_tensor_tmp_initial = agent.normalize_obs(inputs_norm_tensor_tmp_initial, class_instance.cfg.env)
-
-        inputs_norm_tensor_tmp = inputs_norm_tensor_tmp.to(dtype=torch.float32)
-        inputs_norm_tensor_tmp_initial = inputs_norm_tensor_tmp_initial.to(dtype=torch.float32)
-
-        aim_output_visualize = agent.aim_discriminator.forward(inputs_norm_tensor_tmp).detach().cpu().numpy().flatten()
-
-        M = cv2.getPerspectiveTransform(world_coordinate, image_coordinates)
-        background_image = mpimg.imread(background_image_path)
-        positions = np.stack((x_debug, y_debug), axis=1)
-        pixel_positions = cv2.perspectiveTransform(positions.reshape(-1, 1, 2), M).reshape(-1, 2)
-        X_debug_pixel, Y_debug_pixel = np.meshgrid(pixel_positions[:, 0], pixel_positions[:, 1])
-        aim_outputs_tmp_pixel = cv2.perspectiveTransform(aim_output_visualize.reshape(-1, 1, 2), M)
+    M = cv2.getPerspectiveTransform(world_coordinate, image_coordinates)
+    background_image = mpimg.imread(background_image_path)
+    positions = np.stack((x, y), axis=1)
+    pixel_positions = cv2.perspectiveTransform(positions.reshape(-1, 1, 2), M).reshape(-1, 2)
 
 
-        c = ax.pcolormesh(pixel_positions[:, 0], pixel_positions[:, 1],
-                          aim_output_visualize.reshape(x_debug.shape[0], -1).T,
-                          cmap='RdBu_r', alpha=0.8)
-        selected_goal_pixel = cv2.perspectiveTransform(selected_goal.reshape(-1, 1, 2), M).reshape(-1, 2).flatten()
-        ax.scatter(selected_goal_pixel[0], selected_goal_pixel[1], marker="*", c='black', s=15, label='goal')
-        ax.imshow(background_image)
-        ax.axis('off')
-        ax.set_aspect('equal', 'box')
-        # I don't want to show the pixel values of the aim in the colorbar. therefore the following line is necessary. (but dont visualize it in first graph therefore alpha-
-        fig.colorbar(c, ax=ax)
-        fig.tight_layout()
-        fig.savefig(f'{savedir_w_name}_ep_goal_fixed{goal_index}.png',bbox_inches='tight')
-        plt.close(fig)
+    with torch.no_grad():
+        observes = torch.as_tensor(np.concatenate([initial_states, goal_xy], axis=-1),
+                                   device=class_instance.device).float()  # [num_test_points^2, dim*2]
+        if class_instance.cfg.normalize_rl_obs is not None:
+            observes = agent.normalize_obs(observes, class_instance.cfg.env)
+        aim_outputs = agent.aim_discriminator.forward(observes).detach().cpu().numpy()  # [num_test_points^2, 1]
 
-        c2 = ax3.pcolormesh(x_debug, y_debug, aim_output_visualize.reshape(x_debug.shape[0], -1).T,cmap='RdBu_r', alpha=0.8)
+    v_min, v_max = aim_outputs.min(), aim_outputs.max()
 
+    aim_outputs = np.reshape(aim_outputs, [num_test_points, num_test_points])
 
-        ax3.plot(map.ox, map.oy, ".k")
-        ax3.scatter(selected_goal[0], selected_goal[1], marker="*", c='black', s=15, label='goal')
-        #ax2.imshow(background_image)
-        ax3.axis('off')
-        ax3.set_aspect('equal', 'box')
-        fig3.colorbar(c2, ax=ax2)
-        fig3.tight_layout()
-        fig3.savefig(f'{savedir_w_name}_ep_goal_fixed_no_back{goal_index}.png',bbox_inches='tight')
-        plt.close(fig3)
+    fig, ax = plt.subplots()
+    #ax.plot(map.ox, map.oy, ".k")
 
-
-     ### Another plot
-    del aim_output_visualize
-
-    aim_output_visualize = agent.aim_discriminator.forward(inputs_norm_tensor_tmp_initial).detach().cpu().numpy().flatten()
-    c = ax2.pcolormesh(pixel_positions[:, 0], pixel_positions[:, 1],aim_output_visualize.reshape(x_debug.shape[0], -1).T,
-                      cmap='RdBu_r', alpha=0.8)
-    initial_state_pixel = cv2.perspectiveTransform(initial_state.reshape(-1, 1, 2), M).reshape(-1, 2).flatten()
-    #Initial Position scatter plot
-    ax2.scatter(initial_state_pixel, initial_state_pixel, marker="*", c='black', s=15, label='goal')
-    ax2.imshow(background_image)
-    ax2.axis('off')
-    ax2.set_aspect('equal', 'box')
+    c = ax.contourf(pixel_positions[:, 0], pixel_positions[:, 1], aim_outputs, cmap='RdBu', vmin=v_min, vmax=v_max,alpha=0.6)
     fig.colorbar(c, ax=ax)
-    fig2.tight_layout()
-    fig2.savefig(f'{savedir_w_name}_ep_init_fixed_no_back.png', bbox_inches='tight')
+    ax.imshow(background_image)
+    if initial_state.ndim == 1:
+        ax.scatter(initial_state[0], initial_state[1], marker="*", c='black', s=10, label='Current_position')
+    else:
+        for t in range(initial_state.shape[0]):
+            ax.scatter(initial_state[t, 0], initial_state[t, 1], marker="*", c=str(1. - t / initial_state.shape[0]),
+                       s=10, label='s_' + str(t))
+    if class_instance.cfg.env in ['AntMazeSmall-v0', "PointUMaze-v0"]:
+        obstacle_point_x = np.array([-2, 6, 6, -2])
+        obstacle_point_y = np.array([2, 2, 6, 6])
+        ax.plot(obstacle_point_x, obstacle_point_y, c='black')
+    elif class_instance.cfg.env == "PointSpiralMaze-v0":
+        obstacle_point_x = np.array([-2, 6, 6, -6, -6, 10, 10, -10, -10, 10, 10, -2, -2])
+        obstacle_point_y = np.array([2, 2, 6, 6, -6, -6, -10, -10, 10, 10, -2, -2, 2])
+        ax.plot(obstacle_point_x, obstacle_point_y, c='black')
+    elif class_instance.cfg.env in ["PointNMaze-v0"]:
+        obstacle_point_x = np.array([-2, 6, 6, -2])
+        obstacle_point_y = np.array([2, 2, 6, 6])
+        ax.plot(obstacle_point_x, obstacle_point_y, c='black')
+        obstacle_point_x = np.array([10, 2, 2, 10])
+        obstacle_point_y = np.array([10, 10, 14, 14])
+        ax.plot(obstacle_point_x, obstacle_point_y, c='black')
+    else:
+        pass
 
-    plt.close(fig2)
 
-    c2 = ax4.pcolormesh(x_debug, y_debug, aim_output_visualize.reshape(x_debug.shape[0], -1).T,cmap='RdBu_r', alpha=0.8)
-    ax4.plot(map.ox, map.oy, ".k")
-    ax4.scatter(initial_state[0], initial_state[1], marker="*", c='black', s=15, label='goal')
-    #ax2.imshow(background_image)
-    ax4.axis('off')
-    ax4.set_aspect('equal', 'box')
-    fig4.colorbar(c2, ax=ax2)
-    fig4.tight_layout()
-    fig4.savefig(f'{savedir_w_name}_ep_init_fixed.png', bbox_inches='tight')
-    plt.close(fig4)
+    ax.axis('off')
+    ax.set_aspect('equal', 'box')
 
-    plt.close('all')
-
-
-
-
-    # for goal_index, selected_goal in enumerate(goal_candidates):
-    #     repeated_state_debug = torch.tile(torch.tensor(selected_goal).to(class_instance.device), [virtual_diffusion_goals_debug.shape[0], 1])
-    #     inputs_norm_tensor_tmp = torch.hstack((virtual_diffusion_goals_debug, repeated_state_debug))
-    #     if class_instance.cfg.normalize_rl_obs:
-    #         inputs_norm_tensor_tmp = agent.normalize_obs(inputs_norm_tensor_tmp, class_instance.cfg.env)
-    #
-    #     inputs_norm_tensor_tmp = inputs_norm_tensor_tmp.to(dtype=torch.float32)
-    #     aim_output_visualize = agent.aim_discriminator.forward(inputs_norm_tensor_tmp).detach().cpu().numpy().flatten()
-    #
-    #
-    #
-    #
-    #     M = cv2.getPerspectiveTransform(world_coordinate, image_coordinates)
-    #     background_image = mpimg.imread(background_image_path)
-    #     positions = np.stack((x_debug, y_debug), axis=1)
-    #     pixel_positions = cv2.perspectiveTransform(positions.reshape(-1, 1, 2), M).reshape(-1, 2)
-    #     X_debug_pixel, Y_debug_pixel = np.meshgrid(pixel_positions[:, 0], pixel_positions[:, 1])
-    #     aim_outputs_tmp_pixel = cv2.perspectiveTransform(aim_output_visualize.reshape(-1, 1, 2), M)
-    #     fig, ax = plt.subplots()
-    #     C = np.stack((X_debug_pixel, Y_debug_pixel), axis=-1).reshape(-1, 2)
-    #     S = np.concatenate([np.zeros((C.shape[0], 7)), C], axis=1)
-    #     c = ax.pcolormesh(pixel_positions[:, 0], pixel_positions[:, 1], aim_outputs_tmp_pixel.reshape(x_debug.shape[0], -1),
-    #                       cmap='RdBu_r', alpha=0.8)
-    #     background_image = mpimg.imread(background_image_path)
-    #     # Create the plot and display the background image
-    #     ax.imshow(background_image)
-    #     ax.axis('off')
-    #     ax.axis('tight')
-    #     ax.set_aspect('equal', 'box')
-    #     fig.colorbar(c)
-    #
-    #     plt.savefig('/home/erdicitymos/Desktop/DiffusionOutpace/saved_log/PointSpiralMaze-v0/2025.04.04/aim_reward_env.png',bbox_inches='tight')
-    #
-    #     fig, ax = plt.subplots()
-    #     C = np.stack((X_debug, Y_debug), axis=-1).reshape(-1, 2)
-    #     S = np.concatenate([np.zeros((C.shape[0], 7)), C], axis=1)
-    #     c = ax.pcolormesh(x_debug, y_debug, aim_outputs_tmp_pixel.reshape(x_debug.shape[0], -1), cmap='RdBu_r', alpha=0.8)
-    #     map = UMaze()
-    #     ax.plot(map.ox, map.oy, ".k")
-    #     ax.axis('off')
-    #     ax.axis('tight')
-    #     ax.set_aspect('equal', 'box')
-    #     ax.scatter(0, 8, marker="*", color='red', edgecolor='k', s=65)
-    #     ax.scatter(0, 0, marker="*", color='orange', edgecolor='k', s=65)
-    #     fig.colorbar(c)
-
+    fig.tight_layout()
+    fig.savefig(f'{savedir_w_name}_ep_init_fixed_no_back.png', bbox_inches='tight')
+    plt.close(fig)
 
 
 
